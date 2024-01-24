697a44435c956808561d352d134063568260a74e
root <Part4@github.com>
2024-01-24 10:04:58 -0800

    deleting more things (again)

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(text):
     for char in text:
-        if 97 <= ord(char) <= 122:
+        if 97 <= ord(char) and ord(char) <= 122:
             char = chr(ord(char) - 32)
-            print("{}".format(char), end="")
-        else:
-            print("{}".format(char), end="")
+        print("{}".format(char), end="")
     print("")
