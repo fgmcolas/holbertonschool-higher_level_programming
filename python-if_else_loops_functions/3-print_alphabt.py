18fda6c4b36d1d171ef232908ecd7f56ffd59dd7
root <Part4@github.com>
2024-01-24 09:46:59 -0800

    deleting a character

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
     if chr(letter) != 101 and chr(letter) != 113:
-        print("{}".format(chr(letter))", end="")
+        print("{}".format(chr(letter)), end="")
