9b39448c7d37bb0c452e1a501ad6fb3ab31a47e4
root <Part4@github.com>
2024-01-24 09:24:38 -0800

    back to old way of print and deleting whitespaces

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ lastdigit = abs(number) % 10
 if number < 0:
     lastdigit = -lastdigit
 
-print("Last digit of {} is {}".format(number, lastdigit), end=" ")
+print(f"Last digit of {number} is {lastdigit} ", end="")
 
 if lastdigit > 5:
     print("and is greater than 5")
