963dee342bdc41a3033935ed77b0c6e3f7214d9e
root <Part4@github.com>
2024-01-24 09:57:14 -0800

    changing syntax

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for d1 in range(10):
     for d2 in range(d1 + 1, 10):
-        print(f"{d1}{d2}", end=", " if d1 != 8 else "\n")
+        print("{}{}".format(d1, d2), end=", " if d1 != 8 else "\n")
