efbb22dcf21f3e3b26e981760578b744b0e72e6d
root <Part4@github.com>
2024-01-24 09:42:24 -0800

    adding abs

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-lastdigit = number % 10
+lastdigit = abs(number) % 10 * (-1 if number < 0 else 1)
 
 print(f"Last digit of {number} is {lastdigit}", end=" ")
 
