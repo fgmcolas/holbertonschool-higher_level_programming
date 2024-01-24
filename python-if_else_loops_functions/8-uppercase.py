13236ca95577877718b4b76900c77414d2ccc66e
root <Part4@github.com>
2024-01-24 09:59:39 -0800

    changing syntax

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(text):
     for char in text:
         if 97 <= ord(char) <= 122:
             upper = chr(ord(char) - 32)
-            print(f"{upper}", end="")
+            print("{upper}".format(char), end="")
         else:
-            print(f"{char}", end="")
+            print("{}".format(char), end="")
     print("")
