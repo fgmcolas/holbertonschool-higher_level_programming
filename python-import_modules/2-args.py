5feb6d03532e49f82fd74a2a4e68fbe806303f67
root <Part4@github.com>
2024-01-25 03:10:21 -0800

    deleting a whitespace

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == "__main__":
-    argnumb =  len(sys.argv) - 1
-    if (argnumb == 0):
-        print("0 arguments.")
-    elif (argnumb == 1):
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(argnumb))
-    for arg in range(1, len(sys.argv)):
-        print("{}: {}".format(arg, sys.argv[arg]))
+if __name__ != "__main__":
+    exit()
+
+argnumb = len(sys.argv) - 1
+if (argnumb == 0):
+    print("0 arguments.")
+elif (argnumb == 1):
+    print("1 argument:")
+else:
+    print("{} arguments:".format(argnumb))
+for arg in range(1, len(sys.argv)):
+    print("{}: {}".format(arg, sys.argv[arg]))
