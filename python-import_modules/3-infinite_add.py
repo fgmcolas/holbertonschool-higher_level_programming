31c51fded682e71acedc557e5d2cf89852acf6e8
root <Part4@github.com>
2024-01-25 02:25:13 -0800

    adding README + all excercices working localy

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+result = 0
+for arg in range(1, len(sys.argv)):
+    result += int(sys.argv[arg])
+print("{}".format(result))
