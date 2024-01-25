2a617747fb4d10ba8ab9c2e25e05b4e200680c12
root <Part4@github.com>
2024-01-25 02:30:02 -0800

    adding a variable

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,5 @@ a = 1
 b = 2
 from add_0 import add
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = (a, b)
+    print("{} + {} = {}".format(a, b, result))
