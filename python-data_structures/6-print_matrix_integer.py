cc3f411ade462c7ed6fda1ec0e8da7076d9127f4
root <Part4@github.com>
2024-01-31 06:08:18 -0800

    changing print function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
-            if i <= len(row):
-                print("{:d}".format(row[i]), end=" ")
-            else:
-                print("{:d}".format(row[i]), end="")
+            print("{:d}".format(row[i]), end=" " if i != len(row) - 1 else "")
         print()
