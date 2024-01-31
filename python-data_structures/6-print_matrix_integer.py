b1d606c86b9b3f5c3c5a2abc05b13840aed3f5bf
root <Part4@github.com>
2024-01-31 05:48:03 -0800

    adding README, and all exercice working localy with pycodestyle

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i <= len(row):
+                print("{}".format(row[i]), end=" ")
+            else:
+                print("{}".format(row[i]), end="")
+        print()
