ba6a8029d18928fc8825faff60e6e794dc1a3221
root <Part4@github.com>
2024-02-05 09:09:10 -0800

    adding whitespaces

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,11 @@ Define a square class
 
 class Square:
     """Define the square attribute"""
+
     def __init__(self, size=0):
-        """Initialize a new square"""
+        """
+        Initialize a new square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be a integer")
         if size < 0:
