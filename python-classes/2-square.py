d426405d4e95e0874eca8172c019b504e10ebdfa
root <Part4@github.com>
2024-02-05 09:06:23 -0800

    adding a commentary for def __init__

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,6 +7,7 @@ Define a square class
 class Square:
     """Define the square attribute"""
     def __init__(self, size=0):
+        """Initialize a new square"""
         if not isinstance(size, int):
             raise TypeError("size must be a integer")
         if size < 0:
