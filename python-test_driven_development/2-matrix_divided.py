124af2a547cb6e7ca211c33054eb5bb7802f956e
root <Part4@github.com>
2024-02-07 14:50:55 -0800

    deleting is None

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -29,7 +29,7 @@ def matrix_divided(matrix, div):
                         "of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, (int, float)) or is None:
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
