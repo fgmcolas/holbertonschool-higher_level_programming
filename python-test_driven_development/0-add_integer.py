57a69f5c649058964ae2d5840a5725cb730463e1
root <Part4@github.com>
2024-02-07 05:26:26 -0800

    edditing number on comment's lines + one case for the test file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+
+"""
+Module for addition
+Add two integers
+Cast float into ints
+"""
+
+
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-
-    Args:
-        a (int or float): The first integer.
-        b (int or float, optional): The second integer. Defaults to 98.
-
-    Returns:
-        int: The sum of a and b, casted to integer.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
+    """add two integers
+    Returns: sum (int)
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
