f7f5f1a7168532172c1cc4ba8c47eebe79ff19ff
root <Part4@github.com>
2024-02-07 14:48:01 -0800

    adding whitespaces

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,7 +25,7 @@ def matrix_divided(matrix, div):
             not all(isinstance(row, list) for row in matrix) or
             not all(isinstance(item, (int, float))
                     for row in matrix for item in row)):
-        raise TypeError("matrix must be a matrix (list"
+        raise TypeError("matrix must be a matrix (list "
                         "of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
