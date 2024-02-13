caca4c5bfd8d197e10b2a428b3050840f4a40f5e
root <Part4@github.com>
2024-02-13 15:58:25 -0800

    removing if condition and adjusting return

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -12,6 +12,4 @@ def is_same_class(obj, a_class):
         If obj is exactly an instance of a_class: True
         Else: Flase
     """
-    if type(obj) == a_class:
-        return True
-    return False
+    return type(obj) is a_class:
