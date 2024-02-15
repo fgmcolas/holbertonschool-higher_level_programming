ddb5cbd29ee3bc41c5487b0df3bc0681e92756c2
root <Part4@github.com>
2024-02-14 17:46:12 -0800

    removing whitespaces and improving indentation

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Write an object to a text file using JSON representation"""
-    with open(filename, "w") as newfile: 
-            json.dump(my_obj, newfile)
+    with open(filename, "w") as newfile:
+        json.dump(my_obj, newfile)
